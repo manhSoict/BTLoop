8314fea8a9e6e338733705083ffb5bd412e71240
nguyendaniel1307 <quangnguyenv1307@gmail.com>
2023-07-09 17:09:47 +0700

    test tree combobox

--- src/main/java/com/entities/Category.java
+++ src/main/java/com/entities/Category.java
@@ -13,7 +13,62 @@ public class Category implements Serializable {
     String info;
     int idNumber;
 
-    public void Category() {
+    int treeLevel = 0;
+
+    public Category() {
         children = new ArrayList<>();
+    }
+
+    public Category(String name) {
+        children = new ArrayList<>();
+        this.name = name;
+    }
+
+    public Category getParent() {
+        return parent;
+    }
+
+    public void setParent(Category parent) {
+        this.parent = parent;
+    }
+
+    public List<Category> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Category> children) {
+        this.children = children;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public int getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(int idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public int getTreeLevel() {
+        return treeLevel;
+    }
+
+    public void setTreeLevel(int treeLevel) {
+        this.treeLevel = treeLevel;
     }
 }
