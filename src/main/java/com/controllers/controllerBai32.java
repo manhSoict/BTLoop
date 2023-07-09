0743cf389cb579ece1b92c59925dcb9f670cf42b
ManhSoict <hsa1bacyenthanh@gmai.com>
2023-07-09 18:01:00 +0700

    update controller 32

--- src/main/java/com/controllers/controllerBai32.java
+++ src/main/java/com/controllers/controllerBai32.java
@@ -11,6 +11,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,14 +19,18 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class controllerBai32 implements Initializable {
+        public boolean more3choice;
+
         @FXML
         public ComboBox<String> GradeC1,GradeC2,GradeC3,GradeC4,GradeC5;
         @FXML
-        public Label creatnewquestion;
+        public Label morechoice;
         @FXML
         private CheckBox showQuestion;
         @FXML
         private ScrollPane morequestion;
+        @FXML
+        private AnchorPane Choice3,Choice4,Choice5,PaneLabel;
         @FXML
         private TableView<Question> questionTable;
 
@@ -33,14 +38,43 @@ public class controllerBai32 implements Initializable {
         ObservableList<String> list = FXCollections.observableArrayList("None","100%","90%","83.33333%","80%","75%","70%","66.66667%","60%","50%","40%","33.33333%","30%","25%","20%","16.66667%","14.28571%","12%","11.11111%","10%","5%","-5%");
         @Override
         public void initialize(URL arg0, ResourceBundle arg1) {
+            more3choice = false;
             GradeC1.setItems(list);
             GradeC2.setItems(list);
-//            GradeC3.setItems(list);
-//            GradeC4.setItems(list);
-//            GradeC5.setItems(list);
+            GradeC3.setItems(list);
+            GradeC4.setItems(list);
+            GradeC5.setItems(list);
+            Choice3.setVisible(false);
+            Choice4.setVisible(false);
+            Choice5.setVisible(false);
+            PaneLabel.setLayoutX(285);
+            PaneLabel.setLayoutY(551);
+
+        }
+        @FXML
+        public void clickBalnkmore3choice(MouseEvent event) throws IOException {
+            if (more3choice==false) {
+                Choice3.setVisible(true);
+                Choice4.setVisible(true);
+                Choice5.setVisible(true);
+                PaneLabel.setLayoutX(285);
+                PaneLabel.setLayoutY(895);
+                more3choice = true;
+                morechoice.setText("DELETE 3 ADDED CHOICES");
+            }
+            else{
+                Choice3.setVisible(false);
+                Choice4.setVisible(false);
+                Choice5.setVisible(false);
+
+                PaneLabel.setLayoutX(285);
+                PaneLabel.setLayoutY(551);
+                more3choice = false;
+                morechoice.setText("BLANK FOR MORE 3 CHOICES");
+            }
         }
 
-        @FXML
+    @FXML
         public void openBai32(MouseEvent event) throws IOException {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/baitaplonoop/Bai32.fxml"));
             Parent root = loader.load();
